53c3b832f683ed3fa979998d4d2da0208a87b3b2
Travis T <travistokuyama@gmail.com>
2018-09-16 23:07:59 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,7 +86,6 @@ def all_supplies_in_holidays(holiday_hash)
     end
   end
 end
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
